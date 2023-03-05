a9e64113182a4daac2a1f2cd2efbcf599f7a84d8
Sarmad Gardezi <mail@sarmadgardezi.com>
2023-03-05 19:21:49 +0500

    config css added in about page header

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -6,6 +6,93 @@ import metadata from '../layout/metadata.json';
 import Head from 'next/head';
 import AboutSection from '@/components/about-top'
 import { ContentWrapper } from '../layout/ContentWrapper.tsx';
+import { darkTheme, styled } from '../config/stitches.config';
+
+
+
+const StyledSection = styled('section', {
+  '.hero-content': {
+    paddingTop: `calc(${HEADER_HEIGHT}px + 2rem)`,
+    paddingBottom: '4rem',
+    backgroundColor: '$secondary50',
+    borderBottom: '2px solid $border',
+
+    '@md': { py: `calc(${HEADER_HEIGHT}px + 4rem)` },
+
+    [`.${darkTheme} &`]: { backgroundColor: '$secondary50' },
+
+    '> div': {
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'flex-start',
+      justifyContent: 'flex-start',
+      columnGap: '4rem',
+      rowGap: '1rem',
+
+      '@md': { flexDirection: 'row', rowGap: 'unset' },
+
+      '.portrait': {
+        flexShrink: 0,
+        width: '18.75rem',
+        height: '18.75rem',
+        border: '2px solid $border',
+        borderRadius: '$default',
+
+        '@md': { height: '22.5rem', marginBottom: '0' },
+      },
+
+      '.hero-introduction': {
+        h1: { fontWeight: 500, color: '$primary900', my: '2rem' },
+        p: { marginBottom: '2rem' },
+      },
+    },
+  },
+
+  '.hero-cards': {
+    marginTop: '2rem',
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'flex-start',
+    justifyContent: 'flex-start',
+    rowGap: '1rem',
+    columnGap: '1rem',
+    flexWrap: 'wrap',
+
+    '@md': {
+      alignItems: 'center',
+      justifyContent: 'flex-start',
+      columnGap: '3rem',
+      rowGap: 'unset',
+      marginTop: '-3rem',
+    },
+
+    '.hero-card-item': {
+      '.title': {
+        display: 'block',
+        fontSize: '$mini',
+        fontWeight: 500,
+        lineHeight: '$mini',
+        textTransform: 'uppercase',
+        marginBottom: '0.5rem',
+      },
+
+      '.value': {
+        display: 'flex',
+        alignItems: 'center',
+        columnGap: '0.5rem',
+        background: '$surface100',
+        padding: '0.5rem 1rem',
+        borderRadius: '$default',
+        transition: 'all 1s ease',
+
+        '&:hover': {
+          background: '$primary50',
+          cursor: `url("data:image/svg+xml;utf8,<svg xmlns='http://www.w3.org/2000/svg' width='40' height='48' viewport='0 0 100 100' style='fill:black;font-size:24px;'><text y='50%'>🔍</text></svg>") 16 0, auto`,
+        },
+      },
+    },
+  },
+});
 
 const About: NextPage = () => {
   const {titleSuffix } = metadata
@@ -15,7 +102,10 @@ const About: NextPage = () => {
  <Head>
         <title key="title">About {metadata.titleSuffix}</title>
 			</Head>
-      <ContentWrapper>
+      <StyledSection id="hero-section">
+      <div className="hero-content">
+        <ContentWrapper>
+
           <div className="hero-introduction">
             <h1>
               Writes code.
@@ -26,9 +116,11 @@ const About: NextPage = () => {
               As a web developer and former product manager, I love engineering
               as much as shipping great products.
             </p>
+          
           </div>
         </ContentWrapper>
-
+      </div>
+        </StyledSection>
       <VStack as="section" alignItems="flex-start" w="full" spacing={3}>
         <AboutSection />
       <Heading as='h5' size='sm'>This website is designed and developed by Sarmad Gardezi</Heading>
