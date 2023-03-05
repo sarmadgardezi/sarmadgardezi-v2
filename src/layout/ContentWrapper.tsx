31ed8f0d3b1b7072985c5ef92b36679bc540dd5b
Sarmad Gardezi <mail@sarmadgardezi.com>
2023-03-05 19:01:33 +0500

    contentwrapper and stiches link

---
+++ src/layout/ContentWrapper.tsx
@@ -0,0 +1,26 @@
+import { styled } from '@config/stitches.config';
+
+export const ContentWrapper = styled('div', {
+  width: '100%',
+  maxWidth: '52rem',
+  margin: '0 auto',
+  px: '$pagePadding',
+});
+
+export const LegalContentWrapper = styled('div', {
+  width: '100%',
+  maxWidth: '52rem',
+  margin: '0 auto',
+  px: '$pagePadding',
+  paddingTop: `4rem`,
+  paddingBottom: '10rem',
+
+  hr: { my: '4rem' },
+  h1: { color: '$primary900', marginBottom: '2rem' },
+  p: {
+    color: '$subtext',
+    marginBottom: '0.5rem',
+    maxWidth: '37.5rem',
+    '> strong': { display: 'block', marginTop: '2rem' },
+  },
+});
